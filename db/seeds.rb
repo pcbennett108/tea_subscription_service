527c7b907507d41b21bea35519d4a111b3933763
pcbennett108 <pcbennett42@gmail.com>
2024-01-30 22:50:49 -0700

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+tea1 = Tea.create!(title: "Blueberry Green Tea", description: "A scrumptious evening tea", temperature: 208, brew_time: 15, price: 25.0)
+tea2 = Tea.create!(title: "Black Tea", description: "A scrumptious evening tea", temperature: 208, brew_time: 15, price: 26.00)
+customer = Customer.create!(first_name: "Lisa", last_name: "Simpson", email: "lisa@gmail.com", address: "123 Fake St.")
+
+subscription = Subscription.create!(title: "Blueberry Green Tea", price: "#{tea1.price}", frequency: "3 months", customer: customer, tea: tea1, sub_status: 1)
